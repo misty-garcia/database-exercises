94a2b5ab0a304f9a0d6ae49dc01c6a99e57b5eb4
Misty <misty@codeup.com>
2019-09-03 15:29:27 -0500

    add table exercises

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,18 @@
+use employees;
+
+show tables;
+
+show create table employees;
+describe employees; 	
+
+-- 6. numeric type: emp_no
+-- 7. string type: first_name, last_name 
+-- 8. date type: birth_date, hire_date
+
+show create table departments;
+describe departments;
+
+-- 9. employees table holds employee num and departments table holds department number, and dept_manager shows which employee is in which department
+
+show create table dept_manager;
+describe dept_manager;
