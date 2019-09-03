7851ecbb812713190de4d539b789b661592695fa
Misty <misty@codeup.com>
2019-09-03 15:40:44 -0500

    added comments to table exercises

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -1,7 +1,9 @@
 use employees;
 
+-- show tables in currently selected db
 show tables;
 
+-- exploring the employees db
 show create table employees;
 describe employees; 	
 
@@ -9,10 +11,12 @@ describe employees;
 -- 7. string type: first_name, last_name 
 -- 8. date type: birth_date, hire_date
 
+-- explore the departments db
 show create table departments;
 describe departments;
 
 -- 9. employees table holds employee num and departments table holds department number, and dept_manager shows which employee is in which department
 
+-- explore the dept_manager db
 show create table dept_manager;
 describe dept_manager;
