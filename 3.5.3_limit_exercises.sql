6d9be197a6a7ac09bd5cfbcdb0b44a784c02c5be
Misty <misty@codeup.com>
2019-09-04 12:26:52 -0500

    remove excess lines

--- 3.5.3_limit_exercises.sql
+++ 3.5.3_limit_exercises.sql
@@ -1,25 +1,3 @@
-use employees;
-
-select * from employees where first_name in ("Irena", "Vidya", "Maya") order by first_name;
-select * from employees where first_name in ("Irena", "Vidya", "Maya") order by first_name, last_name;
-select * from employees where first_name in ("Irena", "Vidya", "Maya") order by last_name, first_name;
-
-select * from employees where last_name like 'E%' order by emp_no;
-
-describe employees;
-select * from employees where hire_date between '1990-01-01' and '1999-12-31';
-select * from employees where birth_date like '%12-25';
-select * from employees where last_name like '%q%';
-select * from employees where (first_name="Irena" or first_name="Vidya"or first_name="Maya") and gender = 'M';
-
-select * from employees where last_name like 'E%' or last_name like '%E' order by emp_no DESC;
-select * from employees where last_name like 'E%E' order by emp_no DESC;
-
-select * from employees where hire_date between '1990-01-01' and '1999-12-31' and birth_date like '%12-25'
-order by birth_date, hire_date DESC;
-
-select * from employees where last_name like '%q%' and last_name not like '%qu%';
-
 select distinct title from titles;
 
 select distinct last_name from employees order by last_name desc limit 10;
@@ -32,4 +10,4 @@ select * from employees where hire_date between '1990-01-01' and '1999-12-31' an
 order by birth_date, hire_date DESC
 limit 5 offset 45;
 
--- to get to the correct page of results, subtract one from the desired page number and then multiply by the limit
+-- to get to the correct page of results, subtract one from the desired page number and then multiply by the limit and then use that as the offset 
